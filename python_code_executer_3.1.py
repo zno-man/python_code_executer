6a769391ffacd631d73362e434d4bfd34ee682a0
zno-man <59720725+zno-man@users.noreply.github.com>
2020-09-27 20:38:02 +0530

    Update python_code_executer_3.1.py

--- python_code_executer_3.1.py
+++ python_code_executer_3.1.py
@@ -1,18 +1,19 @@
 print("enter the text file name where the code is kept(no need to add .txt):",end='')
 name=input()
 name+=".txt"
-try:
+try:                                                                            #opens the file and handles error
     aslkdjhfaskdjhf=open(name,'r')
 except:
     print("error\n please check the file name(do not add .txt at the end)")
     print("\npress enter to exit...")
     a=input()
     exit()
+    
 code=""
 for i in aslkdjhfaskdjhf:
-    code+=i
-print(code)     
-exec(code)
-print("\npress enter to exit...")
+    code+=i                                                                     #gets the code from the file as a single string
+print(code)                                                                     #displays the actual code                                                          
+exec(code)                                                                      #executes the code
+print("\npress enter to exit...")                                               #waits for input/enter key
 a=input()	
 aslkdjhfaskdjhf.close()
